d97b969fe22dee823dbeaa68ba8649c5422e0db0
Sasaki1994 <b.92421933411@gmail.com>
2020-03-12 16:29:20 +0900

    エラーハンドリング

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -1,7 +1,27 @@
 $(function(){
-  
+  function buildFailHTML(){
+    html = `\
+    <div class="response">\
+      <div class="response__body">\
+      　<p>質問の探索に失敗しました。</br>質問の箇所と質問内容を確認してください。</p>\
+        </br>
+      </div>\
+    </div>`;
+    return html
+  }
 
-  function buildHTML(text){
+  function buildNoResHTML(){
+    html = `\
+    <div class="response">\
+      <div class="response__body">\
+      　<p>類似の質問が見つかりませんでした</p>
+        </br>
+      </div>\
+    </div>`;
+    return html
+  }
+
+  function buildResHTML(text){
     html = `\
     <div class="response">\
       <div class="response__body">\
@@ -30,8 +50,18 @@ $(function(){
       dataType: 'json'
     })
     .done(function(event){
-      for(let text of event["text"]) {
-        response = buildHTML(text);
+      if (event["success"] == true){
+        if (event["n_item"] > 0){
+          for(let text of event["text"]) {
+            let response = buildResHTML(text);
+            $('.main').append(response);
+          }
+        }else{
+          let response = buildNoResHTML();
+          $('.main').append(response);
+        }
+      }else{
+        let response = buildFailHTML();
         $('.main').append(response);
       }
       $("html,body").animate({scrollTop:$('.response').offset().top - 90});

2ad2021330516eeabc048b311f3b9bf640a15afc
Sasaki1994 <b.92421933411@gmail.com>
2020-03-12 19:51:41 +0900

    questions_controller test done

---
+++ spec/controllers/questions_controller_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+describe QuestionsController do
+  describe 'GET #index' do
+    it 'renders the :index template' do
+      get :index
+      expect(response).to render_template :index
+    end
+  end
+
+  describe 'POST #search' do
+    it 'renders json as success' do
+      post :search, params: {question: {url: "https://master.tech-camp.in/curriculums/1183", text: "hamlが分からない"}}
+      json = JSON.parse(response.body)
+      expect(json['success']).to eq(true)
+    end
+
+    it 'renders json as fail when url is "" ' do
+      post :search, params: {question: {url: "", text: "hamlが分からない"}}
+      json = JSON.parse(response.body)
+      expect(json['success']).to eq(false)
+    end
+
+    it 'renders json as fail when text is "" ' do
+      post :search, params: {question: {url: "https://master.tech-camp.in/curriculums/1183", text: ""}}
+      json = JSON.parse(response.body)
+      expect(json['success']).to eq(false)
+    end
+
+    it 'renders json as fail when url is wrong format ' do
+      post :search, params: {question: {url: "https:///hoge.com", text: "hamlが分からない"}}
+      json = JSON.parse(response.body)
+      expect(json['success']).to eq(false)
+    end
+
+
+  end
+      
+end 
